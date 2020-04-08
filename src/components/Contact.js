dd2b8c1a78545f7018f9df0cc743b3aeaea2f799
Julien Martos <ju_sxm@hotmail.fr>
2020-04-08 12:16:03 +0200

    modif de l'intendation et ajout props.online dans Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import "./contact.css"
-import PropTypes from 'prop-types';
+import React from "react";
+import "./contact.css";
+import PropTypes from "prop-types";
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.statut,
+    };
+  }
 
-/*parent*/
-const Contact = (props) => 
-    <div className = "Contact">
-        <img src = {props.avatar} alt ="" className = "avatar"/>
-        <div className = "status">
-            <h4 className = "name">{props.name}</h4>
-            {props.statut ? <span className = "status-online"></span> : <span className = "status-offline"></span>}
-            <p className = "status-text">{props.statut ? "online" : "offline"}</p>          
+  render() {
+    return (
+      <div
+        className="Contact"
+        onClick={(event) => {
+          const newStatut = !this.state.online;
+          this.setState({ online: newStatut });
+        }}
+      >
+        <img src={this.props.avatar} alt="" className="avatar" />
+        <div className="status">
+          <h4 className="name">{this.props.name}</h4>
+          {this.state.online ? (
+            <span className="status-online"></span>
+          ) : (
+            <span className="status-offline"></span>
+          )}
+          <p className="status-text">
+            {this.state.online ? "online" : "offline"}
+          </p>
         </div>
-    </div>
-
-Contact.propTypes = {
-    name : PropTypes.string,
-    statut : PropTypes.bool,
+      </div>
+    );
+  }
 }
 
+Contact.propTypes = {
+  name: PropTypes.string,
+  statut: PropTypes.bool,
+};
 
 export default Contact;
